c971f27d10f36fc67ce78d8b046f88236a9fe514
rums1605 <singhbiswajit79@gmail.com>
2017-06-23 20:28:18 +0530

    Add files via upload

---
+++ ReduxSimpleStarter/src/index.js
@@ -0,0 +1,77 @@
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
+import SearchBar from './components/search_bar';
+import YTSearch from 'youtube-api-search';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
+
+const API_KEY='AIzaSyCI951KkN08u2crDrsylduA0Vqsv4Q6eqw';
+/*
+YTSearch({key:API_KEY, term:'surfboards'},(data) =>{
+    console.log(data);
+}
+);
+
+
+const App=() =>{
+
+return (
+<div>
+ <SearchBar/>
+ </div>
+
+);
+
+
+}*/
+
+class App extends Component {
+
+
+constructor(props)
+{
+super(props);
+
+
+this.state={ videos: [],
+             selectedVideo:null  
+           };
+
+
+this.videoSearch('surfboards');
+}
+
+videoSearch(term)
+{
+
+
+YTSearch({key:API_KEY, term:term},(videos) =>{
+    this.setState({
+        videos:videos,
+        selectedVideo:videos[0]});
+
+    console.log(data);
+}
+);
+
+}
+
+
+
+render()
+{
+return (
+   <div>
+    <SearchBar onSearchTermChange={term=>this.videoSearch(term)} />
+    <VideoDetail video={this.state.selectedVideo} />
+    <VideoList onVideoSelect={selectedVideo=>this.setState({selectedVideo})}
+               videos={this.state.videos}/>
+   </div>
+);
+}
+
+
+}
+
+ReactDOM.render(<App/>,document.querySelector('.container'))
+
